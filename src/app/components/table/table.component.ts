52c2e6893234951fc41df347b7a78921ad1fa670
Henricus Wouter de Vos (Group) <henricuswouter.devos@global.ntt>
2020-02-18 14:28:49 +0200

    first push

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./table.component.sass']
 })
 export class TableComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
