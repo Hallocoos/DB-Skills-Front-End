d8aae77e4d160ee2470ee04258979a21db98ca94
Henricus Wouter de Vos (Group) <henricuswouter.devos@global.ntt>
2020-02-19 09:32:51 +0200

    fixed table and displaying data

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { GetDataService } from '../../services/get-data.service';
+
+import { EmployeeDetails } from '../../models/Employee';
+import { EmployeeSkills } from '../../models/EmployeeSkills';
 
 @Component({
   selector: 'app-table',
@@ -6,9 +10,33 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./table.component.sass']
 })
 export class TableComponent implements OnInit {
-  constructor() { }
+  employees: EmployeeDetails[];
+
+//   employees = [{
+//     id: 'number',
+//     name: 'string',
+//     surname: 'string',
+//     jobTitle: 'string',
+//     teamOffice: 'string',
+//     lineManager: 'string'
+//   },
+//   {
+//     id: 'number',
+//     name: 'string',
+//     surname: 'string',
+//     jobTitle: 'string',
+//     teamOffice: 'string',
+//     lineManager: 'string'
+//   }
+// ];
+
+  constructor(private getData: GetDataService) { }
 
   ngOnInit(): void {
+    this.getData.getAllEmployees().subscribe(allEmployees => {
+      this.employees = allEmployees;
+      console.log('allEmployees: ', this.employees);
+    });
   }
 
 }
---
+++ src/app/models/Employee.ts
@@ -0,0 +1,8 @@
+export class EmployeeDetails {
+id: number;
+name: string;
+surname: string;
+jobTitle: string;
+teamOffice: string;
+lineManager: string;
+}
