f3072a66af8dc4aeff30b4f1f1bae48286fe35fa
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-02-19 13:40:59 +0200

    added upload route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QueryBarComponent } from './query-bar/query-bar.component';
 import { TableComponent } from './table/table.component';
+import { FileUploadComponent } from './query-bar/file-upload/file-upload.component';
 
 const routes: Routes = [
   { path: 'query', component: QueryBarComponent },
-  { path: 'tables', component: TableComponent }
+  { path: 'tables', component: TableComponent },
+  { path: 'upload', component: FileUploadComponent }
 ];
 
 @NgModule({
